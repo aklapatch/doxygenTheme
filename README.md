8dd41490ed5b6bac3e12e1252584e4a0d9e08389
Aaron Klapatch <sniper96a@gmail.com>
2019-02-21 14:12:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Doxygen Theme
+
+A doxygen theme intended to look modern and clean.
+
+## Installing
+Copy the `CustomDoxygen.css` into the same folder as your doxygen configuration file, and set `HTML_EXTRA_STYLESHEET` to `CustomDoxygen.css`.
+
---
+++ example.h
@@ -0,0 +1,23 @@
+
+
+/// \file
+/// This is an example file for this example project.
+
+
+/// An example struct with a couple members
+struct ExampleStruct {
+	
+	int IntMember; ///< An integer member
+	
+	double DoubleMember; ///< A double member
+	
+	/// Sets both members to zero
+	ExampleStruct() : IntMember(0), DoubleMember(0.0) { }
+};
+
+/// An example function prototype
+/// \param ExampleParam An int * parameter
+/// \param ExampleArg A double type argument
+char * exampleFunction( int * ExampleParam, double ExampleArg);
+	
+	
